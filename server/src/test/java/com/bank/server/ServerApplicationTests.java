40dd7a02efe557ad5c5381bf58f8c711f9c85e16
Nataliya435 <nataliya435@yandex.ru>
2023-02-19 12:32:50 +0300

    2-эврика(1)

---
+++ server/src/test/java/com/bank/server/ServerApplicationTests.java
@@ -0,0 +1,14 @@
+package src.test.java.com.bank.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
